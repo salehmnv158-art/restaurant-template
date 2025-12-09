eac60996e974d6d8b7250f3e6e82c9ab3df1c579
salehmnv158-art <salehmnv158@gmail.com>
2025-12-09 21:15:50 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+navreserve.addEventListener('mouseover', ()=>{
+    navreserve.classList.add('navbar_reserve-active');
+    navreserve.classList.remove('navbar_reserve-passive');
+})
+
+navreserve.addEventListener('mouseleave', ()=>{
+    navreserve.classList.remove('navbar_reserve-active');
+    navreserve.classList.add('navbar_reserve-passive');
+})
+
+let menubutton = document.querySelector('.menu_site_button');
+
+menubutton.addEventListener('mouseover', ()=>{
+    menubutton.classList.add('menu_site_button-active');
+    menubutton.classList.remove('menu_site_button-passive');
+})
+
+menubutton.addEventListener('mouseleave', ()=>{
+    menubutton.classList.remove('menu_site_button-active');
+    menubutton.classList.add('menu_site_button-passive');
+})
+
+let contreserve = document.querySelector('.reservation_ins_cont_book_a_table');
+
+contreserve.addEventListener('mouseover', ()=>{
+    contreserve.classList.add('reservation_ins_cont_book_a_table-active');
+    contreserve.classList.remove('reservation_ins_cont_book_a_table-passive');
+})
+
+contreserve.addEventListener('mouseleave', ()=>{
+    contreserve.classList.remove('reservation_ins_cont_book_a_table-active');
+    contreserve.classList.add('reservation_ins_cont_book_a_table-passive');
+})
+
+
+let navbutact = document.querySelector('.navbar_activate_button');
+let navbutdeact = document.querySelector('.navbar_deactivate_button');
+let navbarright = document.querySelector('.navbar_right');
+
+navbutact.addEventListener('click', ()=>{
+    navbutact.classList.add('display_none');
+    navbutdeact.classList.remove('display_none');
+    navbarright.classList.remove('display_none');
+})
+
+navbutdeact.addEventListener('click', ()=>{
+    navbutdeact.classList.add('display_none');
+    navbutact.classList.remove('display_none');
+    navbarright.classList.add('display_none');
+})
